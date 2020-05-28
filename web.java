2c50b7d066a6308901e6a76c012f869346f5f858
Nicole <nicolelian97@gmail.com>
2020-05-28 13:05:34 +0800

    Updated Web.java file
    This is according to the new CSV. Other changes that needs to be made is removing the total cost. & also now that there are items with less than $10 and even decimal points, dont think the substring method works anymore

--- web.java
+++ web.java
@@ -77,7 +77,7 @@ public class web {
         while ((row = csvReader.readLine()) != null) {
             String[] data = row.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
             String name = data[3];
-            String phone_number = data[20];
+            String phone_number = data[25];
             System.out.println(name);
             System.out.println(phone_number);
             String final_message = templating(data, header);
@@ -138,21 +138,21 @@ public class web {
         //ArrayList<String> order_line = new ArrayList<String>();
         double total_cost = 0;
         String orderline = "";
-        recipient_name = data[24];
-        address = data[21]+ " " + data[22]; 
+        recipient_name = data[29];
+        address = data[26].substring(0,data[26].length())+ " " + data[27]; 
         System.out.println(address);
-        delivery_date = data[19].substring(1,data[19].length()-1);
+        delivery_date = data[24].substring(1,data[24].length()-1);
         name = data[3];
-        delivery_cost = data[17];
-        message=data[27];
-        for (int i = 0; i < 10; i++){
+        delivery_cost = data[22];
+        message=data[32];
+        for (int i = 0; i < 15; i++){
             int index = i + 4;
             if(data[index].isEmpty()){
                 continue;
             }
             else{
                 qty = data[index];
-                price = header[index].substring(1,3);
+                price = header[index].substring(1,4);
                 item_name = header[index].substring(8);
                 orderline += (qty + " * " + item_name + " $" + price + "\n");
                 total_cost += Double.parseDouble(qty) * Double.parseDouble(price);
@@ -161,7 +161,7 @@ public class web {
         }
         total_cost += Double.parseDouble(delivery_cost);
         // String temp = String.format("THIS IS A TESTER MESSAGE PLEASE IGNORE \n Thank you for ordering with TiapTiapWithSoph, here is your order summary 🥳 - \n\nName:\n%s \n\n📆 Time of Order:\n%s \n\n📝 Order:\n%s\n\n🏡 Delivery Address:\n%s\n\n🚚 Delivery cost & date:\n$%s,%s\n\n💵 Total cost:\n%.2f\nPlease make your payment to 90089066 via paylah or paynow. Once you have made the payment, please send a screenshot to the number together with this order form to complete the order process 🎉\n",name,time_stamp,orderline,address,delivery_cost,delivery_date,total_cost);
-        String temp = String.format("Thank you for ordering with TiapTiapWithSoph <3, here is your order summary - \n\nSender's Name:\n%s \n\nRecipient's Name:\n%s \n\nOrder:\n%s\n\nDelivery Address:\n%s\n\nDelivery cost & date:\n$%s,%s\n\nTotal cost:\n%.2f\n\nMessage:\n%s\n\nPlease make your payment to 90089066 via paylah or paynow. You can also bank transfer to POSB 051160410. Once you have made the payment, please send a screenshot to the number together with this order form to complete the order process <3 \n",name,recipient_name,orderline,address,delivery_cost,delivery_date,total_cost,message);
+        String temp = String.format("Hey there! This is Nicole, Sophia's Daughter! I am contacting you on behalf of my mother to confirm your order! Thank you for ordering with TiapTiapWithSoph <3, here is your order summary - \n\nSender's Name:\n%s \n\nRecipient's Name:\n%s \n\nOrder:\n%s\n\nDelivery Address:\n%s\n\nDelivery cost & date:\n$%s,%s\n\nTotal cost:\n%.2f\n\nMessage:\n%s\n\nPlease make your payment to 90089066 via paylah or paynow. You can also bank transfer to POSB 051160410. Once you have made the payment, please send a screenshot to the number together with this order form to complete the order process <3 \n",name,recipient_name,orderline,address,delivery_cost,delivery_date,total_cost,message);
         //String template = "Name:\n{name}\n📆 Time of Order:\n{time_stamp}\n📝 Order:\n{qty} * (item_name) $(price)\n🏡 Delivery Address:\n{address}\n🚚 Delivery cost:\n{delivery_cost}\n💵 Total cost:\n{total_cost}\nPlease make your payment to 90089066 via paylah or paynow. Once you have made the payment, please send a screenshot to the number together with this order form to complete the order process 🎉";
         return temp;
     }
